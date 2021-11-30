729ad059912fcdeb2f500b45b2be320ee740f250
Andrii Maliuta <quadr988@gmail.com>
2021-11-30 19:15:34 +0200

    hostman cloud

--- src/main/java/com/anma/sb/dbdeneratorsb/models/Person.java
+++ src/main/java/com/anma/sb/dbdeneratorsb/models/Person.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class Person {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "person_id")
     private long id;
     private String name;
